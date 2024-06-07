faa1dedc038ea6868ac1455040a80245eb5d3a53
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2024-06-07 12:31:37 -0700

    added routing setup and create login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import Login from "./pages/LoginPage";
+import Signup from "./pages/SignupPage";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/SignupPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SignupComponent from "../features/auth/components/Signup";
+import Signup from "../features/auth/components/Signup";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <SignupComponent />
+    </div>
+  );
+};
+
+export default SignupPage;
